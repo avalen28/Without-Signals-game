87b081745e586f039e61af2810336d24d64c4ba3
Alberto Valenzuela <avalen28@gmail.com>
2022-11-27 17:42:34 +0100

    adds tyranid class and methods

---
+++ js/tyranid.js
@@ -0,0 +1,21 @@
+class Tyranid {
+  constructor() {
+    this.health = 100;
+    this.strength = 20;
+    this.x = 400;
+    this.y = -100;
+    this.width = 100;
+    this.height = 100;
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(damage) {
+    this.health = this.health - damage;
+    if (this.health > 0) {
+      console.log(`The Tyranid receives ${damage} points of damage!`);
+    } else {
+      console.log("The Tyranid dies in horrible screams!");
+    }
+  }
+}
