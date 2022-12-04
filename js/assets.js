0a2b4d1ff5a249d9a0699f0a79fd3b753661d9fa
Alberto Valenzuela <avalen28@gmail.com>
2022-12-04 19:53:01 +0100

    adds sound test. WIP on visuals

---
+++ js/assets.js
@@ -0,0 +1,8 @@
+const soldierImg = new Image();
+soldierImg.src = "../images/soldier2.png";
+
+const tyranidImg = new Image();
+tyranidImg.src = "../images/nid2.png";
+
+const emperorSound = new Audio();
+emperorSound.src = "../sounds/FOR THE EMPEROR.mp3";
