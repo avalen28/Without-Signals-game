73fb44df6c72be47431efd21ff6ae6dc29f185dc
Alberto Valenzuela <avalen28@gmail.com>
2022-12-06 21:57:52 +0100

    adds random generator quotes and when you attack/are attacked have a random quote

--- js/soldier.js
+++ js/soldier.js
@@ -8,17 +8,29 @@ class Soldier {
     this.height = 300;
     this.movement = true;
     this.healthNumbers = 1;
+    this.poolQuotes = [
+      "A storm of rending claws attacks with fury!.",
+      "The Tyranid jumps and bite you neck!.",
+      "the creature spits acid on you.",
+    ];
+    this.quoteSelected = 0;
   }
-
+  _selectRandomQuote() {
+    let max = this.poolQuotes.length;
+    this.quoteSelected = Math.floor(Math.random() * max);
+  }
   _generateDamage(initialStrengh) {
     let finalDmg = Math.floor(Math.random() * 10) * initialStrengh;
     return finalDmg;
   }
   receiveDamage(damage) {
+    this._selectRandomQuote();
     this.health = this.health - damage;
     let soldierStatus = "";
     if (this.health > 0) {
-      soldierStatus = `Emperor... PROTECTS ME! You receive ${damage} points of damage!`;
+      soldierStatus = `${
+        this.poolQuotes[this.quoteSelected]
+      } You receive ${damage} points of damage!`;
     } else {
       soldierStatus = `You receive ${damage} points of damage and die in the name of the Emperor...`;
     }
@@ -27,16 +39,16 @@ class Soldier {
   healthHp() {
     let soldierHealthQuote = "";
     if (this.health >= 50) {
-      soldierHealthQuote = "I can do this all day... DIE XENO!";
+      soldierHealthQuote = "My faith is my shield anything can hurt me!";
     } else if (this.health < 50 && this.healthNumbers === 1) {
       this.healthNumbers = 0;
       let recover = Math.floor(Math.random() * 10 + 40);
       this.health = this.health + recover;
       soldierHealthQuote = `Helix Gauntlet Activated. You recover ${recover} health points! Helix Gauntlet remaining:${this.healthNumbers}`;
-    } else {
-      soldierHealthQuote =
-        "My Helix Gauntlet is empty...but my faith is my shield!";
-    }
+    } //else {
+    //soldierHealthQuote =
+    //"My Helix Gauntlet is empty...but my faith is my shield!";
+    //}
     return soldierHealthQuote;
   }
   moveLeft() {
--- js/tyranid.js
+++ js/tyranid.js
@@ -5,17 +5,29 @@ class Tyranid {
     this.y = -200;
     this.width = 250;
     this.height = 200;
+    this.poolQuotes = [
+      "You shoot with your blessed Boltgun.",
+      "You attack with your knife.",
+      "Your existence is a blasphemy... DIE XENO!",
+    ];
+    this.quoteSelected = 0;
+  }
+  _selectRandomQuote() {
+    let max = this.poolQuotes.length;
+    this.quoteSelected = Math.floor(Math.random() * max);
   }
   _generateDamage(initialStrengh) {
     let finalDmg = Math.floor(Math.random() * 10) * initialStrengh;
-
     return finalDmg;
   }
   receiveDamage(damage) {
+    this._selectRandomQuote();
     this.health = this.health - damage;
     let tyranidStatus = "";
     if (this.health > 0) {
-      tyranidStatus = `FOR THE EMPEROR! the Tyranid receives ${damage} points of damage!`;
+      tyranidStatus = `${
+        this.poolQuotes[this.quoteSelected]
+      } The Tyranid receives ${damage} points of damage!`;
     } else {
       tyranidStatus = `The Tyranid recives ${damage} points of damage and dies in horrible screams!`;
     }
