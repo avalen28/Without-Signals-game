e30a5a373a7dea94c934596d46e52abe3f3a4c26
Alberto Valenzuela <avalen28@gmail.com>
2022-12-07 17:58:58 +0100

    solving images url

--- js/assets.js
+++ js/assets.js
@@ -1,17 +1,17 @@
 const soldierImg = new Image();
-soldierImg.src = "../images/soldier2.png";
+soldierImg.src = "./images/soldier2.png";
 
 const tyranidImg = new Image();
-tyranidImg.src = "../images/nid2.png";
+tyranidImg.src = "./images/nid2.png";
 
 const emperorSound = new Audio();
-emperorSound.src = "../sounds/FOR THE EMPEROR.mp3";
+emperorSound.src = "./sounds/FOR THE EMPEROR.mp3";
 
 const introSound = new Audio();
-introSound.src = "../sounds/music-intro.mp3";
+introSound.src = "./sounds/music-intro.mp3";
 
 const soldierFightImg = new Image();
-soldierFightImg.src = "../images/soldierRight.png";
+soldierFightImg.src = "./images/soldierRight.png";
 
 const tyranidFightImg = new Image();
-tyranidFightImg.src = "../images/tyranid.png";
+tyranidFightImg.src = "./images/tyranid.png";
